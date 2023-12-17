40a77c3822692731a0db96502718828872c87c90
ph04 <tacc010203@gmail.com>
2023-12-17 18:23:47 +0100

    Trying to understand how the frontend works

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -7,14 +7,11 @@ export default {}
 
 <template>
 
-	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">Example App</a>
-		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-	</header>
+	<!-- <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+		<p class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">WASAPhoto</p>
+	</header> -->
 
-	<div class="container-fluid">
+	<!-- <div class="container-fluid">
 		<div class="row">
 			<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
 				<div class="position-sticky pt-3 sidebar-sticky">
@@ -56,11 +53,14 @@ export default {}
 				</div>
 			</nav>
 
-			<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-				<RouterView />
-			</main>
+			
 		</div>
-	</div>
+	</div> -->
+
+	<!-- <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"> -->
+	<main>
+		<RouterView />
+	</main>
 </template>
 
 <style>
---
+++ webui/src/views/LoginView.vue
@@ -0,0 +1,67 @@
+<script>
+    export default {
+        data: function() {
+            return {
+                errormsg: null,
+                loading: false,
+                some_data: null,
+            }
+        },
+        methods: {
+            async refresh() {
+                this.loading = true;
+                this.errormsg = null;
+                try {
+                    let response = await this.$axios.get("/session");
+                    this.some_data = response.data;
+                } catch (e) {
+                    this.errormsg = e.toString();
+                }
+                this.loading = false;
+            },
+        },
+        mounted() {
+            this.refresh()
+        }
+}
+</script>
+
+<template>
+    <div class="everything">
+        <header>
+            <p class="header">WASAPhoto</p>
+        </header>
+
+        <div class="center-div">
+            <div class="user-icon-div">
+                <img class="user-icon" src="assets/user.svg">
+            </div>
+
+            <div class="bar-section-div">
+                <input class="bar" placeholder="Enter your username!">
+
+                <button class="button">boh</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .everything {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 1920px;
+        height: 1080px;
+
+        margin: 0px;
+    }
+
+    .header {
+        font-weight: 800;
+        font-size: 250%;
+
+        color: #485696;
+    }
+</style>
